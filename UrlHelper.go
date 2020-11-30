9ccfb5e479c59c6d6f597a41c9d3778648e994a7
Mete Mertkan <metemertkan@gmail.com>
2020-11-30 09:17:58 +0100

    Add files via upload

---
+++ UrlHelper.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func getPokemon(url string) string {
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	return string(body)
+}
